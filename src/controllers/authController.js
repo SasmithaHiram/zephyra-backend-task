6f5669283767e8f465bd8151b9148635ca908e1a
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-07-10 20:43:54 +0530

    bug-fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -19,26 +19,28 @@ const signup = async (req, res) => {
 };
 
 const signin = async (req, res) => {
-    const {username, password} = req.body;
+  const { username, password } = req.body;
 
-    try {
-        const user = await User.findOne({username});
+  try {
+    const user = await User.findOne({ username });
 
-        if(!user) return res.status(404).json({message: 'Invalid credentials'});
+    if (!user) return res.status(404).json({ message: "Invalid credentials" });
 
-        const match = await bcrypt.compare(password, user.password);
+    const match = await bcrypt.compare(password, user.password);
 
-        if(!match) return res.status(404).json({message: 'Invalid credentials'});
+    if (!match) return res.status(404).json({ message: "Invalid credentials" });
 
-        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
-        res.status(200).json({token});
-    } catch (error) {
-        res.status(500).json({message: 'Signin error', error});
-    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Signin error", error });
+  }
 };
 
 const protectedRoute = (req, res) => {
-    res.status(200).json({message: 'Access to protected data granted'});
+  res.status(200).json({ message: "Access to protected data granted" });
 };
 
-exports = {signup, signin, protectedRoute};
+export { signup, signin, protectedRoute };
